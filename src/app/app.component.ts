3ecbb83c0c2933c66ed65484a3f67af9b59b9938
jupmorenor <jupmorenor@gmail.com>
2021-11-12 16:39:10 -0500

    hotfix: Se elimina menú quemado y se optimiza el build de desarrollo, se agrega check_commits en drone y release con versión. #1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,13 +7,13 @@ declare let gtag: Function;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
   loadRouting = false;
   environment = environment;
   loadingRouter: boolean;
-  title = 'configuracion-cliente';
+  title = 'resoluciones-cliente-v2';
   constructor(
     private router: Router,
     private userService: UserService 
@@ -27,9 +27,9 @@ export class AppComponent implements OnInit {
                   );
           }
        }
-    )}
+    )
+  }
 
-  
   ngOnInit(): void {
     const oas = document.querySelector('ng-uui-oas');
 
