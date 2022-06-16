830f3b96d79a7bbc92076bc9068a4eb3ca42cf83
jupmorenor <jupmorenor@gmail.com>
2022-06-16 08:30:37 -0500

    devops: Se actualiza versión de node y se ajusta scope de token. #40

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -30,7 +30,7 @@ export const environment = {
     AUTORIZATION_URL: 'https://autenticacion.portaloas.udistrital.edu.co/oauth2/authorize',
     CLIENTE_ID: 'e36v1MPQk2jbz9KM4SmKhk8Cyw0a',
     RESPONSE_TYPE: 'id_token token',
-    SCOPE: 'openid email',
+    SCOPE: 'openid email role documento',
     REDIRECT_URL: 'http://localhost:4200/',
     SIGN_OUT_URL: 'https://autenticacion.portaloas.udistrital.edu.co/oidc/logout',
     SIGN_OUT_REDIRECT_URL: 'http://localhost:4200/',
